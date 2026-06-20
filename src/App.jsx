f5242ed10352d47923a7182dd06a760e950ec3c7
H-Mehmood <hassanig567@gmail.com>
2026-06-20 18:15:51 +0500

    completed counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // state
+  const [count, setCount] = useState(0);
+  
+  // event handlers
+  const handleSum = (e) => {
+    e.preventDefault();
+    // increase count by 1
+    setCount(count + 1);
+  }
+  const handleMinus = (e) => {
+    e.preventDefault();
+    // prevent count from going below 0
+    if(count === 0) return;
+    // decrease count by 1
+    setCount(count - 1);
+  }
+  const handleRefresh = (e) => {
+    e.preventDefault();
+    // reset count to 0    
+    setCount(0);
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="App">
+        <div className="container">
 
-      <div className="ticks"></div>
+          {/* linear gradient card */}
+          <div className="total_amount_card" style={{ background: "#57C785", background: "linear-gradient(90deg, rgba(87, 199, 133, 1) 0%, rgba(237, 221, 83, 1) 100%)" }}>
+            {/* refresh icon */}
+            <div className="right">
+              <svg onClick={handleRefresh} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
+              <path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"></path>
+              </svg>
+            </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            {/* total amount */}
+            <div className="card_text">
+              <h3 className="total_amount_heading">{count}</h3>
+            </div>
+          </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          {/* buttons */}
+
+          <form>
+            <div className="button_collection">
+              <Stack spacing={2} direction="row" className="btn_stack">
+                <Button onClick={handleSum} variant="contained" className="btn_plus">+</Button>
+                <Button onClick={handleMinus} variant="contained" className="btn_minus">-</Button>
+              </Stack>
+            </div>
+          </form>
+
+        </div>
+      </div>
     </>
   )
 }
